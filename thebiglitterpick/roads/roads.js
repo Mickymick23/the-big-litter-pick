9991eba48be0db610f19df17b0dc0f9a64e67b48
Mickymick23 <gy23mpgg@leeds.ac.uk>
2026-09-08 19:27:37 +0100

    basemap2

--- thebiglitterpick/roads/roads.js
+++ thebiglitterpick/roads/roads.js
@@ -17,7 +17,8 @@ function initialize() {
 	// BASEMAP
 	// add dark basemap
 	L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	    attribution: 'Tiles &copy; Esri'
+	    attribution: 'Tiles &copy; Esri',
+		className: 'dark-basemap'
 	}).addTo(map);
 	
 	// BOUNDARY
